ddf100b014cbc77beb4e2ad4b009172ea5efa256
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2023-03-07 03:12:36 +0530

    Fix roll up bundle compression and bundle size issues

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,12 @@
 import commonjs from "@rollup/plugin-commonjs";
 import globals from "rollup-plugin-node-globals";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
 export default [
   {
     input: "dist/index.js",
-    plugins: [commonjs(), globals()],
+    plugins: [nodeResolve(), commonjs(), globals(), terser()],
     output: {
       name: "NodeRules",
       format: "umd",
